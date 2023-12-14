a8a475dde8b4f7d187d6dd85f184c00e8da82262
Sebastian Stöcker <sebastian.stoecker@uni-marburg.de>
2023-12-14 18:35:29 +0100

    cleanup unused code

--- pkg/mapper/consent_profile.go
+++ pkg/mapper/consent_profile.go
@@ -38,40 +38,6 @@ func MapProfile(consent fhir.Consent) fhir.Consent {
 	return consent
 }
 
-//func InitProfiles() map[string]Profile {
-//
-//	bc := Profile{
-//		Category: []fhir.CodeableConcept{
-//			{
-//				Coding: []fhir.Coding{{System: Of(LoincCategorySystem), Code: Of(LoincCategoryCode)}},
-//			},
-//			{
-//				Coding: []fhir.Coding{{
-//					System: Of("https://www.medizininformatik-initiative.de/fhir/modul-consent/CodeSystem/mii-cs-consent-consent_category"),
-//					Code:   Of("2.16.840.1.113883.3.1937.777.24.2.184")}},
-//			},
-//		},
-//		ConsentPolicy: &ConsentPolicy{
-//			Uri:  Of("urn:oid:2.16.840.1.113883.3.1937.777.24.2.1790"),
-//			Name: Of("Patienteneinwilligung MII|1.6.d"),
-//		},
-//	}
-//
-//	// only mii profile, currently
-//	return map[string]Profile{
-//		MiiProfile: bc,
-//	}
-//}
-
-//func (p *Profile) GetPolicy(templateName string) *ConsentPolicy {
-//	if p.PolicyMatcher.MatchString(templateName) {
-//		return p.ConsentPolicy
-//	}
-//
-//	log.WithField("template", templateName).Error("Unable to determine Consent policy")
-//	return nil
-//}
-
 func Of[E any](e E) *E {
 	return &e
 }
